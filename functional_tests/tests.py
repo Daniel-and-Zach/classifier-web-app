d0e369f87c162da921ce2da52e69b76d89e6ecee
Zachary Huntington-Meath <zjhuntingtonmeath@gmail.com>
2015-04-17 13:22:31 -0400

    Added a simple text box for the user to put in code.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -13,9 +13,18 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
-    # def tearDown(self):
-    #     self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
     def test_open_browser(self):
+        # User Opens browser, and goes to the index.
         self.browser.get(self.live_server_url)
+        # User knows it's the correct page by the title/header.
         self.assertIn('Language Classifier', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Language Classifier', header_text)
+        # User sees a text box to enter code.
+        text_area = self.browser.find_element_by_tag_name('textarea').is_displayed()
+        self.assertTrue(text_area)
+    # def test_browser_header(self):
+    #     self.brower.get(self.)
--- lang_identifier/tests.py
+++ lang_identifier/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from .views import index
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 
-# Create your tests here.
+class HomePageTest(TestCase):
+
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve("/")
+        self.assertEquals = (found.func, index)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+        expected_html = render_to_string('index.html')
+        self.assertEquals(response.content.decode(), expected_html)
+
