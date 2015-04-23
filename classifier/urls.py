5dd49f29399c7b2f308c1a726f19fd2fe13ecdc4
Daniel Newell <dknewell1@gmail.com>
2015-04-23 17:18:06 -0400

    Added a dummy view to test passing data, next we need to add language classifier

--- classifier/urls.py
+++ classifier/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
     url(r'^$', 'lang_identifier.views.index', name='index'),
+    url(r'^analyze/', 'lang_identifier.views.analyze', name='analyze'),
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
--- lang_identifier/views.py
+++ lang_identifier/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 def index(request):
-    if request.method == 'POST':
-        code_reuslt = "python"
-        return redirect('/', {'code_result': code_result})
     return render(request, 'index.html')
+
+def analyze(request):
+    context = {"code_result": "python"}
+    word = {"code_result": request.POST["item_text"]}
+    return render(request, 'index.html', word)
