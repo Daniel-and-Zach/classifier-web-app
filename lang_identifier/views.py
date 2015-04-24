10d469f9f6884feae59f3a752791d7ff8fb85e56
Zachary Huntington-Meath <zjhuntingtonmeath@gmail.com>
2015-04-24 07:49:54 -0400

    We added the ability to use the classifier on the site.

--- lang_identifier/views.py
+++ lang_identifier/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from .predict_language import make_prediction_from_text
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def analyze(request):
-    context = {"code_result": "python"}
+    text = request.POST['item_text']
+    context = {"code_result": make_prediction_from_text(text)}
     word = {"code_result": request.POST["item_text"]}
-    return render(request, 'index.html', word)
+    return render(request, 'index.html', context)
